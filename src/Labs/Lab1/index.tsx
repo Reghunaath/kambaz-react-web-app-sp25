a9baa77e1dd85a0e22fdf691a21b27161eba6c80
Reghunaath <aa.reghunaath@gmail.com>
2025-01-21 01:33:42 -0500

    added links to lab 1

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,8 +2,31 @@ export default function Lab1() {
   return (
     <div id="wd-lab1">
       <h2>Lab 1</h2>
-      <h3>HTML Examples</h3>
-      ...
+      <h3>Links</h3>
+      Name: Reghunaath Ajith Kumar Ahila
+      <br />
+      Class: CS5610.35649.202530
+      <br />
+      <a
+        href="https://a1--reghu-kambaz-react-web-app-sp25.netlify.app/"
+        id="wd-lipsum"
+      >
+        Assignment 1
+      </a>
+      <br />
+      <a
+        href="https://reghu-kambaz-react-web-app-sp25.netlify.app/"
+        id="wd-lipsum"
+      >
+        Main branch
+      </a>
+      <br />
+      <a
+        href="https://github.com/Reghunaath/kambaz-react-web-app-sp25"
+        id="wd-lipsum"
+      >
+        Source Code
+      </a>
       <div id="wd-tables">
         <h4>Table Tag</h4>
         <table border={1} width="100%">
@@ -21,7 +44,7 @@ export default function Lab1() {
               <td>HTML</td>
               <td>2/3/21</td>
               <td>85</td>
-              </tr>
+            </tr>
             <tr>
               <td>Q2</td>
               <td>CSS</td>
@@ -33,6 +56,48 @@ export default function Lab1() {
               <td>JS</td>
               <td>2/15/21</td>
               <td>95</td>
+            </tr>
+            <tr>
+              <td>Q4</td>
+              <td>2HT2ML</td>
+              <td>2/20/21</td>
+              <td>85</td>
+            </tr>
+            <tr>
+              <td>Q5</td>
+              <td>2C2SS</td>
+              <td>2/25/21</td>
+              <td>90</td>
+            </tr>
+            <tr>
+              <td>Q6</td>
+              <td>2J2S</td>
+              <td>3/1/21</td>
+              <td>95</td>
+            </tr>
+            <tr>
+              <td>Q7</td>
+              <td>HTML: Tokyo Drift</td>
+              <td>3/6/21</td>
+              <td>85</td>
+            </tr>
+            <tr>
+              <td>Q8</td>
+              <td>CSS: Tokyo Drift</td>
+              <td>3/11/21</td>
+              <td>90</td>
+            </tr>
+            <tr>
+              <td>Q9</td>
+              <td>JS: Tokyo Drift</td>
+              <td>3/16/21</td>
+              <td>95</td>
+            </tr>
+            <tr>
+              <td>Q10</td>
+              <td>The Fate of Web Development</td>
+              <td>3/21/21</td>
+              <td>90</td>
             </tr>
           </tbody>
           <tfoot>
